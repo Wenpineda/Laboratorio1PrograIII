039f14fa453b6655c1337cc94ae1ed879af019e2
Wenpineda <cp0428032021@unab.edu.sv>
2023-02-22 16:26:54 -0600

    Update

--- Lab1progra/Lab1progra/Program.cs
+++ Lab1progra/Lab1progra/Program.cs
@@ -36,6 +36,9 @@ namespace Lab1progra
                     person.Datos(person.Nombrecompleto, person.Edad);
                     residencia resi = new residencia();
                     resi.localidad(resi.Departamento, resi.Municipio, resi.Edad);
+                    Console.WriteLine(person.GetPerson());
+                    Console.WriteLine(resi.GetResidencia());
+                    Console.ReadLine();
                     break;
                 case 2:
                     Environment.Exit(0);
@@ -64,8 +67,15 @@ namespace Lab1progra
                 WriteLine("INGRESE SUS DATOS POR FAVOR \n");
                 WriteLine("Ingrese su nombre completo: ");
                 Nombrecompleto = ReadLine();
-                WriteLine("Ingrese su edad completo: ");
+                WriteLine("Ingrese su edad porfavor: ");
                 Edad = int.Parse(Console.ReadLine());
+            }
+
+            public String GetPerson()
+            {
+
+                return "\nNombre Completo: " + Nombrecompleto + "\nEdad: " + Edad ;
+
             }
         }
 
@@ -77,64 +87,120 @@ namespace Lab1progra
             public string Departamento { get => departamento; set => departamento = value; }
             public string Municipio { get => municipio; set => municipio = value; }
 
-            //private float[] departaments;// 6 departamentos
-           //private float[] municip; // 8 municipios 
+
+            //string[] Chalatenango = new string[8] { "La Palma", "San Ignacio", "Citala", "La Laguna", "Comalapa", "Potonico", "La Reina", "Tejutla" };
+            //string[] Sonsonate = new string[8] { "Acajutla", "Caluco", "Izalco", "Nahulingo", "Armenia", "Sonzacate", "Nahuizalco", "San Julián" };
+            //string[] Morazan = new string[8] { "Arambala", "Cacaopera", "Chilanga", "Corinto", "El Divisadero", "Joateca", "Jocoro", "Meanguera" };
+            //string[] Usulutan = new string[8] { "Alegría", "Berlín", "California", "Concepción Batres", "El Triunfo", "Ereguayquín", "Estanzuelas", "Jiquilisco" };
+            //string[] SantaAna = new string[8] { "Candelaria de la Frontera", "Coatepeque", "El Congo", "El Porvenir", "Masahuat", "Metapán", "Texistepeques", "San Antonio Pajonal" };
+            //string[] SanMiguel = new string[8] { "Chirilagua", "Ciudad Barrios", "Comacarán", "Quelepa", "Uluazapa", "Sesori", "Moncagua", "Chapeltique" };
 
 
-            public void localidad(string _departamento , string _municipio, int _edad)
+            public void localidad(string _departamento, string _municipio, int _edad)
             {
                 Persona per = new Persona();
-                string[] almacen = new string[6];
-
-
                 this.Departamento = _departamento;
                 this.Municipio = _municipio;
                 this.Edad = _edad;
                 Clear();
-                try
+                WriteLine("-------------------------------");
+                WriteLine("INGRESE LOS DATOS DONDE VIVE \n");
+                string[] opcion = { "Chalatenango", "Sonsonate", "Morazan", "Usulutan", "Santa Ana", "San Miguel" };
+                for (int f = 0; f < opcion.Length; f++)
                 {
-                    WriteLine("-------------------------------");
-                    WriteLine("INGRESE LOS DATOS DONDE VIVE \n");
-                    WriteLine("Ingrese el Departamento al qu epertenece: ");
-                    departamento = ReadLine();
+                    Console.WriteLine($"{f + 1} - {opcion[f]}");
+                }
+                int Depopcion = int.Parse(ReadLine());
+                while (Depopcion > opcion.Length || Depopcion <= 0)
+                {
+                    WriteLine($" La opcion: {Depopcion} que ud ingreso no se encuantra intentrelo de nuevo");
                     WriteLine("Ingrese el municipio al que pertenece ");
-                    municipio = ReadLine();
-                    WriteLine($"La Persona {per.Nombrecompleto} originario/a del municipio de {municipio} \nen el departamento de {departamento} \ncuenta con {per.Edad} años de edad.");
-                    ReadLine();
-
+                    for (int f = 0; f < opcion.Length; f++)
+                    {
+                        Console.WriteLine($"{f + 1} - {opcion[f]}");
+                    }
+                    Depopcion = int.Parse(ReadLine());
                 }
-                catch (Exception)
+
+                Departamento = opcion[Depopcion - 1];
+                Console.WriteLine($"Ingrese el municipio al que pertenece de {Departamento}\n");
+                int Muniopcion = 0;
+                switch (Departamento)
                 {
+                    case "Chalatenango":
+                        string[] Chalatenango = { "La Palma", "San Ignacio", "Citala", "La Laguna", "Comalapa", "Potonico", "La Reina", "Tejutla" };
 
+                        for (int f = 0; f < Chalatenango.Length; f++)
+                        {
+                            Console.WriteLine($"{f + 1} - {Chalatenango[f]}");
+                        }
+                        Muniopcion = int.Parse(ReadLine());
+                        Municipio = Chalatenango[Muniopcion - 1];
+                        break;
+                    case "Sonsonate":
+                        string[] Sonsonate = { "Acajutla", "Caluco", "Izalco", "Nahulingo", "Armenia", "Sonzacate", "Nahuizalco", "San Julián" };
 
+                        for (int f = 0; f < Sonsonate.Length; f++)
+                        {
+                            Console.WriteLine($"{f + 1} - {Sonsonate[f]}");
+                        }
+                        Muniopcion = int.Parse(ReadLine());
+                        Municipio = Sonsonate[Muniopcion - 1];
+                        break;
+                    case "Morazan":
+                        string[] Morazan = { "Arambala", "Cacaopera", "Chilanga", "Corinto", "El Divisadero", "Joateca", "Jocoro", "Meanguera" };
+
+                        for (int f = 0; f < Morazan.Length; f++)
+                        {
+                            Console.WriteLine($"{f + 1} - {Morazan[f]}");
+                        }
+                        Muniopcion = int.Parse(ReadLine());
+                        Municipio = Morazan[Muniopcion - 1];
+                        break;
+                    case "Usulutan":
+                        string[] Usulutan = { "Alegría", "Berlín", "California", "Concepción Batres", "El Triunfo", "Ereguayquín", "Estanzuelas", "Jiquilisco" };
+
+                        for (int f = 0; f < Usulutan.Length; f++)
+                        {
+                            Console.WriteLine($"{f + 1} - {Usulutan[f]}");
+                        }
+                        Muniopcion = int.Parse(ReadLine());
+                        Municipio = Usulutan[Muniopcion - 1];
+                        break;
+                    case "Santa Ana":
+                        string[] SantaAna = { "Candelaria de la Frontera", "Coatepeque", "El Congo", "El Porvenir", "Masahuat", "Metapán", "Texistepeques", "San Antonio Pajonal" };
+
+                        for (int f = 0; f < SantaAna.Length; f++)
+                        {
+                            Console.WriteLine($"{f + 1} - {SantaAna[f]}");
+                        }
+                        Muniopcion = int.Parse(ReadLine());
+                        Municipio = SantaAna[Muniopcion - 1];
+                        break;
+                    case "San Miguel":
+                        string[] SanMiguel = { "Chirilagua", "Ciudad Barrios", "Comacarán", "Quelepa", "Uluazapa", "Sesori", "Moncagua", "Chapeltique" };
+
+                        for (int f = 0; f < SanMiguel.Length; f++)
+                        {
+                            Console.WriteLine($"{f + 1} - {SanMiguel[f]}");
+                        }
+                        Muniopcion = int.Parse(ReadLine());
+                        Municipio = SanMiguel[Muniopcion - 1];
+                        break;
+                    default:
+                        Console.WriteLine("");
+                        break;
                 }
 
-
-
-                //string linea;
-                //departaments = new float[6];
-                //municip = new float[48];
-
-                //Console.WriteLine("");
-                //for (int f = 0; f < 4; f++)
-                //{
-                //    Console.Write("Ingrese el Departamento " + (f + 1) + ": ");
-                //    linea = Console.ReadLine();
-                //    departaments[f] = float.Parse(linea); //Asignamos valores 
-                //}
-                //Console.WriteLine("");
-                //for (int f = 0; f < 4; f++)
-                //{
-                //    Console.Write("Ingrese el municipio: " + (f + 1) + ": ");
-                //    linea = Console.ReadLine();
-                //    municip[f] = float.Parse(linea); //Asignamos 
-                //}
-                //for (int f = 0; f < 4; f++)
-                //{
-                //    Console.WriteLine("Desea agregar otro dato");
-                //}
             }
 
+            public String GetResidencia()
+            {
+
+
+                return "Departamento: " + Departamento + "\nMunicipio: " + Municipio;
+
+            }
 
         }
         
